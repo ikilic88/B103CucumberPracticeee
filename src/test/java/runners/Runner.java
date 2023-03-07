10065cb3027c5059f1d36b5660a6f8e83f7cc133
ibrahim kılıç <ikilic9188@gmail.com>
2023-03-07 23:59:45 +0300

    v1

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class) // test çalıştırıcı notasyonu
 @CucumberOptions( // Seneryoların nerede ve nasıl çalışacağı, hangi raporun kullanılmasıyla alakalı seçenekleri ayarlar
+       plugin = {"html:src/test/resources/reports/cucumberReport.html"},
         features = "src/test/resources/features",
         glue = "stepdefinitions",
         tags = "@gp1",
---
+++ src/test/resources/features/parametre.feature
@@ -0,0 +1,6 @@
+Feature: Amazon testi
+  @gp1
+  Scenario: TC01_amazon_iphone_arama
+    Given kullanici "amazonUrl gider
+    When kullanici "iphone" için arama yapar
+    Then sayfa basliginin "iphone" icerdigini test eder
